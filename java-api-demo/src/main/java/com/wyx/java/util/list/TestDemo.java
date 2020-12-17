1a286927771cb4eccecb8838e7d59a6ec8feace5
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-12-17 17:21:55 +0800

    ADD

---
+++ java-api-demo/src/main/java/com/wyx/java/util/list/TestDemo.java
@@ -0,0 +1,11 @@
+package com.wyx.java.util.list;
+
+/**
+ * @author : Just wyx
+ * @Date : 2020/12/17
+ */
+public class TestDemo {
+	public static void main(String[] args) {
+		System.out.println(0/1000);
+	}
+}
