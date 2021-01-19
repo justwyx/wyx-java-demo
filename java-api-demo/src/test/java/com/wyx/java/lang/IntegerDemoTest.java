3c4342c7838930eb754007f23ff805efbdddef88
yuxiang_wen <wenyvxiang@iiasaas.com>
2021-01-19 15:49:04 +0800

    add

---
+++ java-api-demo/src/test/java/com/wyx/java/lang/IntegerDemoTest.java
@@ -0,0 +1,16 @@
+package com.wyx.java.lang;
+
+import org.junit.Test;
+
+/**
+ * @author : Just wyx
+ * @Date : 2021/1/19
+ */
+public class IntegerDemoTest {
+	@Test
+	public void test() {
+		int x = 1330;
+		int y = 1330;
+		System.out.println(x == y);
+	}
+}
---
+++ java-api-demo/src/test/java/com/wyx/java/util/map/MapCopyTest.java
@@ -0,0 +1,32 @@
+package com.wyx.java.util.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 用于测试浅拷贝深拷贝
+ * @author : Just wyx
+ * @Date : 2021/1/19
+ */
+public class MapCopyTest {
+
+	/**
+	 * 深拷贝test
+	 */
+	@Test
+	public void deepCopyTest() {
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(10, 10);
+		map.put(20, 20);
+		System.out.println("map:" + map);
+
+		Map<Integer, Integer> mapCopy = new HashMap<>();
+		mapCopy.putAll(map);
+		mapCopy.remove(10);
+		System.out.println("map:" + map);
+		System.out.println("mapCopy:" + mapCopy);
+
+	}
+}
